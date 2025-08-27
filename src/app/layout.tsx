a7557f895f5e2f43c764824e6a2914b140cf528c
Jack Pilowsky <jackpilowsky@Mac.lan>
2025-08-27 12:39:01 -0700

    Set up Fustant font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,11 @@
 import type { Metadata } from "next";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
-import { Geist, Geist_Mono } from "next/font/google";
+import "@fontsource/fustat/400.css"; // weight 400 (regular)
+import "@fontsource/fustat/700.css"; // weight 700 (bold)
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`font-fustat antialiased`}
       >
         <div className="min-h-screen bg-white">
           <Header />
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        fustat: ["Fustat", "sans-serif"], // fallback included
+      },
+    },
+  },
+  plugins: [],
+};
